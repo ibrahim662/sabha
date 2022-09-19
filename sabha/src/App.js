79a1261d3d090d643c7610d1183081cb0fabfa66
Ibrahim <74181672+ibrahim662@users.noreply.github.com>
2022-09-19 13:26:31 +0200

    finished project

--- sabha/src/App.js
+++ sabha/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Choise from "./components/choice"
+import "./App.css";
 
 function App() {
+ 
+    var styles = [
+      'background: rgb(214,16,58);'
+      , 'background: linear-gradient(90deg, rgba(214,16,58,1) 0%, rgba(36,9,121,1) 72%, rgba(53,10,115,1) 100%, rgba(0,212,255,1) 100%);'
+      , 'border: 1px solid #3E0E02'
+      , 'color: white'
+      , 'display: block'
+      , 'text-shadow: white'
+      , 'box-shadow: 5px 5px 20px -1px #FFE225'
+      , 'line-height: 40px'
+      , 'text-align: center'
+      , 'font-weight: bold'
+  ].join(';');
+console.log('%c دعواتكم لنا بالخير و الفرج', styles);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://www.google.com" className="App-link">To google</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <Choise />
     </div>
   );
 }
