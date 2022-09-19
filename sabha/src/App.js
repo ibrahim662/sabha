65db6eee5d74cc06431048c11d3d3291334a46ec
Ibrahim <74181672+ibrahim662@users.noreply.github.com>
2022-09-19 15:14:52 +0200

    fixed button display

--- sabha/src/App.js
+++ sabha/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import Choise from "./components/choice"
+import Choise from "./components/choice";
 import "./App.css";
 
 function App() {
- 
-    var styles = [
-      'background: rgb(214,16,58);'
-      , 'background: linear-gradient(90deg, rgba(214,16,58,1) 0%, rgba(36,9,121,1) 72%, rgba(53,10,115,1) 100%, rgba(0,212,255,1) 100%);'
-      , 'border: 1px solid #3E0E02'
-      , 'color: white'
-      , 'display: block'
-      , 'text-shadow: white'
-      , 'box-shadow: 5px 5px 20px -1px #FFE225'
-      , 'line-height: 40px'
-      , 'text-align: center'
-      , 'font-weight: bold'
-  ].join(';');
-console.log('%c دعواتكم لنا بالخير و الفرج', styles);
-  
+  var styles = [
+    "background: rgb(214,16,58);",
+    "background: linear-gradient(90deg, rgba(214,16,58,1) 0%, rgba(36,9,121,1) 72%, rgba(53,10,115,1) 100%, rgba(0,212,255,1) 100%);",
+    "border: 1px solid #3E0E02",
+    "color: white",
+    "display: block",
+    "text-shadow: white",
+    "box-shadow: 5px 5px 20px -1px #FFE225",
+    "line-height: 40px",
+    "text-align: center",
+    "font-weight: bold",
+  ].join(";");
+  console.log("%c دعواتكم لنا بالخير و الفرج", styles);
+
   return (
-     <div>
+    <div>
       <Choise />
     </div>
   );
--- sabha/src/components/infinitTasbih.jsx
+++ sabha/src/components/infinitTasbih.jsx
@@ -1,24 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { Button } from "@mui/material";
-
+import "./tasbih.css";
 function InfinitTasbih() {
-    const [count, setCount] = useState(1);
-const increment = () => {
-    setCount(count + 1)
-}
+  const [count, setCount] = useState(1);
+  const increment = () => {
+    setCount(count + 1);
+  };
   return (
-    <div className="infinit">
-    <CircularProgressbar
-    className="rounds"
-      value={count}
-      text={count}
-      maxValue={count}
-    />
-    <Button variant="outlined" onClick={increment} >سبح</Button>
-  </div>
-  )
+    <div className="group">
+      <div className="infinit">
+        <CircularProgressbar
+          className="rounds"
+          value={count}
+          text={count}
+          maxValue={count}
+        />
+        <Button variant="outlined" onClick={increment}>
+          سبح
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default InfinitTasbih
+export default InfinitTasbih;
